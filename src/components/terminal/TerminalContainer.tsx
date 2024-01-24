20dd04bcb437a81e7dad5766df20d4d2028d1da4
anirudhnbharadwaj <anirudhbharadwaj13@gmail.com>
2024-01-24 22:10:53 +0530

    SPECIALIS REVELIO!!

--- src/components/terminal/TerminalContainer.tsx
+++ src/components/terminal/TerminalContainer.tsx
@@ -12,10 +12,13 @@ import { getSectionAnimation } from '@/styles/animations';
 const TerminalContainer: FC = () => {
   return (
     <Wrapper id = "terminal" {...getSectionAnimation}>
-      <div className="top-50">
+      <div className="top-100">
         <h2 className="relative text-center mb-10 uppercase tracking-[20px] text-3xl ">
           Terminal
         </h2>
+        <h4 className="mb-5 font-mono text-center text-sm uppercase tracking-[10px] text-accent">
+          Specialis Revelio!! 
+        </h4>
         
       <ContainerDiv>
         <TermContainer>
